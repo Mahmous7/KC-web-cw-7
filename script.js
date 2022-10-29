2655a4c5db5c2a47e13dd6ab2b2141104f76a02e
Mahmoud <alimohmmajed5@gmail.com>
2022-10-29 20:19:05 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function BMI(weight, height) {
+  let resuit = weight / (height * height);
+  return resuit;
+}
+console.log(BMI(80, 1.8));
 
+function Status(BodyMass) {
+  if (BodyMass < 18) {
+    return "لديك نقص في الوزن";
+  } else if (BodyMass >= 18.5 && BodyMass <= 25) {
+    return "وزنك صحي";
+  } else {
+    return "لديك زيادة في الوزن";
+  }
+}
+function Calculate() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+  let bmi_value = BMI(weight, height);
+  let desc = Status(bmi_value);
+  let box = document.getElementById("result");
+
+  box.innerText = bmi_value + " == " + desc;
+}
